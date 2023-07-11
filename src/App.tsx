95cd6ceeb5f294a74d1937d267fba0a050ca8474
Kostyantyn Kryoka <42357921+ferbiy@users.noreply.github.com>
2023-07-11 15:38:16 +0300

    task

---
+++ src/App.tsx
@@ -0,0 +1,62 @@
+import React, { useCallback, useState } from "react";
+import { v4 as uuid } from "uuid";
+import styled from "@emotion/styled";
+import { AddInput } from "./components/AddInput";
+import { TodoItem } from "./components/TodoItem";
+import { TodoList } from "./components/TodoList";
+import { Header } from "./components/Header";
+
+const Wrapper = styled.div({
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  width: 300,
+});
+
+const initialData: Todo[] = [
+  {
+    id: uuid(),
+    label: "Buy groceries",
+    checked: false,
+  },
+  {
+    id: uuid(),
+    label: "Reboot computer",
+    checked: false,
+  },
+  {
+    id: uuid(),
+    label: "Ace CoderPad interview",
+    checked: true,
+  },
+];
+
+function App() {
+  const [todos, setTodos] = useState<Todo[]>(initialData);
+
+  const addTodo = useCallback((label: string) => {
+    setTodos((prev) => [
+      {
+        id: uuid(),
+        label,
+        checked: false,
+      },
+      ...prev,
+    ]);
+  }, []);
+
+
+  return (
+    <Wrapper>
+      <Header>Todo List</Header>
+      <AddInput onAdd={addTodo} />
+      <TodoList>
+        {todos.map((todo) => (
+          <TodoItem {...todo} />
+        ))}
+      </TodoList>
+    </Wrapper>
+  );
+}
+
+export default App;
